acf6e9f888b8e15b7ae3138b2594202d62c59bc4
Michael <mirima92@gmail.com>
2025-01-16 21:59:17 -0500

    Add fly function to make plane ascend with spacebar press

--- scripts/plane.gd
+++ scripts/plane.gd
@@ -1,11 +1,17 @@
 extends CharacterBody2D
 
 const GRAVITY: float = 300.0
+const POWER: float = -200.0
 
 func _ready() -> void:
 	pass
 
 func _physics_process(delta: float) -> void:
-	print(velocity)
 	velocity.y += GRAVITY * delta
+	fly()
 	move_and_slide()
+
+
+func fly() -> void:
+	if Input.is_action_just_pressed("fly") == true:
+		velocity.y = POWER
