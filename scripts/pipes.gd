37472e85679751bfe9843e573732e404f81b79e2
Michael <mirima92@gmail.com>
2025-02-02 00:10:55 -0500

    Set a singleton game_manager file for autoload (includes pipe scroll speed)

--- scripts/pipes.gd
+++ scripts/pipes.gd
@@ -2,15 +2,17 @@ extends Node2D
 
 class_name Pipes
 
+const OFF_SCREEN: float = -500
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position.x -= delta * 120.0
+	position.x -= delta * GameManager.SCROLL_SPEED
 	check_off_screen()
 
 
 # in case on_screen_exited() is not fired on minimized game on MacOS
 func check_off_screen() -> void:
-	if position.x < -500:
+	if position.x < OFF_SCREEN:
 		queue_free()
 
 
